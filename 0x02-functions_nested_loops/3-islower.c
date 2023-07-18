f984fbb9409de24b2eaa3477c970f8bb7d952432
engfatmait <eng_fatma_it@yahoo.com>
2023-07-18 14:54:30 -0700

    new commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,17 @@
 
 /**
  * _islower - checks for lower case charater
- * @c: the int value of the character
+ * @c: the ascii value of the character
  *
  * Return: On success 1.
  * other, 0 is returned.
  */
 int _islower(int c)
 {
-	if (c >= 97 || c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
+	_putchar('\n');
 
 }
