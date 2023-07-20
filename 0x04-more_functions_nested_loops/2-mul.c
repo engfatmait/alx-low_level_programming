a7044a2cfa4503368bd464f9cf85e4a39ad25862
engfatmait <eng_fatma_it@yahoo.com>
2023-07-20 11:57:14 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 integers.
+ * @a: first integer
+ * @b: the second integer
+ * Return: mul of two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
