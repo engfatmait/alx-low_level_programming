924661536cc789e33c3e43990e164697be8d6775
engfatmait <eng_fatma_it@yahoo.com>
2023-07-20 12:06:35 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers from 0 to 9
+ * Return always 0
+*/
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		_putchar('/n');
+		i++;
+	}
+
+	return (0);
+}
