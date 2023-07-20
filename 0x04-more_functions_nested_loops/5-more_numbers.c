147b34bcceaa9431f743141a9c6436f4800cebf4
engfatmait <eng_fatma_it@yahoo.com>
2023-07-20 13:10:09 -0700

    new commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,16 @@
 */
 void more_numbers(void)
 {
-	int i, j;
+	int i, ch;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (ch = 0; ch < 15; ch++)
 		{
-			if (j > 9)
-				_putchar((j / 10) + 48);
-			_putchar((j % 10) + 48);
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
 		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
