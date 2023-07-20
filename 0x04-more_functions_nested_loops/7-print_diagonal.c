eb19c0816d0cc386d0e7b7842bf77f5bac8efab5
engfatmait <eng_fatma_it@yahoo.com>
2023-07-20 13:33:20 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_diagonal - print the diagonal line to screen
+ * @n: integer number of times
+ * Return no
+*/
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(92);
+	_putchar('\n');
+}
