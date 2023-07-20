b2c68a6fb39e61bba2428870645d381da6704ce7
engfatmait <eng_fatma_it@yahoo.com>
2023-07-20 13:22:36 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print the line to screen
+ * @n: integer number of times
+ * Return no
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		if (n > 0)
+			_putchar('_');
+		_putchar('\n');
+}
