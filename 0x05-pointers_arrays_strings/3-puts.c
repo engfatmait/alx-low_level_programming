a2b9592ab3525dbeb9dd64f432bbe180918bfadc
engfatmait <eng_fatma_it@yahoo.com>
2023-07-24 12:18:07 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+ * _puts - function prints string to stdout
+ * @str: input string
+ * Return: nothing
+*/
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (*(str + count) != '\0')
+	{
+		_putchar(*(str + count));
+		count++;
+	}
+
+}
