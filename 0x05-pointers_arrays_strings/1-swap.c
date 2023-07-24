48dcb2b9b72ff6c481d79a2f42db091e0f211dc2
engfatmait <eng_fatma_it@yahoo.com>
2023-07-24 11:21:53 -0700

    first commit to function swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - function that swaps the value of 2 integers
+ * @a: one input pointer to integer
+ * @b: another input pointer to integer input to swap
+ * Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+
+
+}
