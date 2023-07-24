a167cef192530ff6567d8eb097f8318ea9487edf
engfatmait <eng_fatma_it@yahoo.com>
2023-07-24 14:01:36 -0700

    new commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _puts2 - prints a string, followed by a new line.
+ * puts2 - prints a string, followed by a new line.
  * @str: input string.
  * Return: no return.
  */
-void _puts2(char *str)
+void puts2(char *str)
 {
 	int count = 0;
 
