37988ad3e74c5ce6e7407bcf0802045df619249b
engfatmait <eng_fatma_it@yahoo.com>
2023-07-24 13:50:54 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+/**
+ * _puts2 - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		else if ((str[count] % 2) == 0)
+		_putchar(str[count]);
+		count++;
+	}
+}
