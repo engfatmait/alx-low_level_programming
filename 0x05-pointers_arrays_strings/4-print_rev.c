29d3b4a3aab704b276605f6e5a0058d30a76fe78
engfatmait <eng_fatma_it@yahoo.com>
2023-07-24 12:54:45 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - function prints stringin reverse
+ * @s: input string
+ * Return: nothing
+*/
+void print_rev(char *s)
+{
+	int len, i = 0;
+
+	len = _strlen(s);
+	i = len;
+	while (i <= len)
+	{
+		if (*(s + i) == '\0')
+			continue;
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+
+}
