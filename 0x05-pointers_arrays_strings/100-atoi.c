7e13ca1c838b2b88338e6b5d4d448a46b06ad334
engfatmait <eng_fatma_it@yahoo.com>
2023-07-25 12:16:50 -0700

    for loop problem

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,17 +6,17 @@
 int _atoi(char *s)
 {
 	int i, str_int = 0;
-	int len;
+	int len, j;
 
 	while (s[i] != '\0')
 	{
 		len++;
 		i++;
 	}
-	for (; i <= len; i++)
+	for (j = 0; j <= len; j++)
 	{
-		if (s[i] >= 48 && s[i] <= 57)
-			str_int = s[i] - 48;
+		if (s[j] >= 48 && s[j] <= 57)
+			str_int = s[j] - 48;
 		return (0);
 	}
 	return (str_int);
