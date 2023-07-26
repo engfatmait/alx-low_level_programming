e7bbae7497495adcec2b99cede17382386dc4a02
engfatmait <eng_fatma_it@yahoo.com>
2023-07-26 10:38:31 -0700

    new fun strcopy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,11 +7,11 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i <= n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for (; i < n; i++)
-		dest[i] = '\0';
+	for (j = i; j < n; j++)
+		dest[j] = '\0';
 	return (dest);
 }
