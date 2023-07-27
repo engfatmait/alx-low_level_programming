5778dc17c358f90c28538b04de99ca51f2071211
engfatmait <eng_fatma_it@yahoo.com>
2023-07-27 14:38:59 -0700

    new commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i <= n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for (j = i; j < n; j++)
+	for (j = i - 1; j < n; j++)
 		dest[j] = '\0';
 	return (dest);
 }
