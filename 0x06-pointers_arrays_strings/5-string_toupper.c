acef9d68137d0e45860efc33e9efca770f5bd510
engfatmait <eng_fatma_it@yahoo.com>
2023-07-27 11:04:20 -0700

    first commit of toupper fun

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] - = 32;
+	}
+}
