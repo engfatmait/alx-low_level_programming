fc9fb31e041a3497c146f29fe3cecd5e69a3702c
engfatmait <eng_fatma_it@yahoo.com>
2023-07-29 06:41:37 -0700

    new commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,17 @@
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: input string
+ * Return: pointer to string
+*/
+char *rot13(char *s)
+{
+	int j;
+
+	while (s[j] != '\0')
+	{
+		if((s[j] >= 97 && s[j] <= 122) || (s[j] >= 65 && s[j] <= 90))
+			s[j] = s[j] + 13;
+	j++;
+	}
+	return (s);
+}
