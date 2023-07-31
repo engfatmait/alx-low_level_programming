c24eb87e37123ccb8c9bf8a2a20e74595318e995
engfatmait <eng_fatma_it@yahoo.com>
2023-07-31 13:59:55 -0700

    first commit of fun print diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: input pointer to array
+ * @size: input size
+ * Return: nothing
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sum = sum + s[i][j];
+		}
+	}
+	printf("%d", sum);
+}
+
