17e5c7f07df73391046726c77fa7fdab75283b9e
engfatmait <eng_fatma_it@yahoo.com>
2023-07-31 13:12:13 -0700

    first commit of print chess

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+/**
+ * _strstr -  function that locates a substring
+ * @haystack: input string
+ * @needle: input substring
+ * Return: pointer to the beginning of the located substring
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+
+		}
+
+	}
+}
