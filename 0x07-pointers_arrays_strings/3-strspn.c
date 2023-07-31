ad0a03c190a9c528245f8a2739c707adc42c23d9
engfatmait <eng_fatma_it@yahoo.com>
2023-07-31 10:33:44 -0700

    first commit of fuction strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,18 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: input string
+ * @accept: sub string to find
+ * Return: number of bytes
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, count = 0, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < '\0'; j++)
+			if (accept[j] == s[i])
+				count++;
+	}
+	return (count);
+}
