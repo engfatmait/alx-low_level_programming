20e47498abcd6b2a670e72d2e48ba61ae15eb71a
engfatmait <eng_fatma_it@yahoo.com>
2023-08-01 19:22:32 -0700

    new commit fun strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,13 +6,15 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, count = 0, j;
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
-			if (accept[j] == s[i])
-				count++;
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
 	}
-	return (count - 1);
+	return (i);
 }
