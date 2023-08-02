a09375c52d08fa4b2695063fa37296ab8fe4cda1
engfatmait <eng_fatma_it@yahoo.com>
2023-08-01 19:47:19 -0700

    first commit of strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,12 +6,22 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	for (i = 0; haystack[i] != '\0'; i++)
+	char *bhaystack;
+	char *pneedle;
+
+	while (*haystack != '\0')
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
-
+			haystack++;
+			pneedle++;
 		}
-
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
 	}
+	return (0);
 }
