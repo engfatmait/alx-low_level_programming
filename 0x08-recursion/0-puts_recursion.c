bdc8cce38fc366a470fc536316bb4a209e65b63a
engfatmait <eng_fatma_it@yahoo.com>
2023-08-02 12:16:17 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ * @s: input string
+ * Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	int i = 0;
+	
+	if (i == 0)
+		_putchar(s[0]);
+	else if (s[i] == '\0')
+		_putchar('\n');
+	_puts_recursion(s[i + 1]);
+
+}
