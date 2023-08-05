e2996b8ac5af3d839b7e83b93a9bad3dbea7acbe
engfatmait <eng_fatma_it@yahoo.com>
2023-08-05 05:27:23 -0700

    new commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,8 @@
  * @argv: array of command line arguments
  * Return: always 0 for success
 */
-int main(argc, char *argv[])
+int main(argc __attribute__((unused)), char *argv[])
 {
 	printf("the name of the program %s\n", argv[0]);
 	return (0);
-
-
 }
