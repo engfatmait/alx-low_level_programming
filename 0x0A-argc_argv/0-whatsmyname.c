0172c9fda3b74cebace9c1b0d0c83c78d245efb0
engfatmait <eng_fatma_it@yahoo.com>
2023-08-05 05:23:39 -0700

    new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the name of a program
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: always 0 for success
+*/
+int main(argc, char *argv[])
+{
+	printf("the name of the program %s\n", argv[0]);
+	return (0);
+
+
+}
