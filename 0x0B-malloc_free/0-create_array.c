db4f6822ca480c90c742d6be0eb947e19620717b
engfatmait <eng_fatma_it@yahoo.com>
2023-08-07 11:28:35 -0700

    check return of malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,8 @@ char *create_array(unsigned int size, char c)
 	if (size <= 0)
 		return (0);
 	arr = malloc(size);
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 	return (arr);
