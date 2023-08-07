954f59f6ead07562da274de74ebccd8772e8dd06
engfatmait <eng_fatma_it@yahoo.com>
2023-08-07 11:14:55 -0700

    first commit to function malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - creates an array of chars,initializes it with a specific char
+ * @size: size of the array
+ * @c: the character used for intialization
+ * Return: null if size = 0 , pointer to the array or null for fail
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size <= 0)
+		return (0);
+	arr = malloc(size);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+
+}
