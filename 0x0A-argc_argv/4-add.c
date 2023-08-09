09b63c5e531833220a9d93899f907dfe90c4d6fd
engfatmait <eng_fatma_it@yahoo.com>
2023-08-09 15:37:31 -0700

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,23 +9,20 @@
 */
 int main(int argc, char *argv[])
 {
-	int i;
-	long int sum = 0;
+	int i, j, add = 0;
 
-	if (argc == 0)
-		printf("%d\n", 0);
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(atol(argv[i])))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			sum += atol(argv[i]);
-		}
+		add += atoi(argv[i]);
 	}
-	printf("%ld\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
