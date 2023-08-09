210a17cef453afbe8ad6d539d7e7dd0720eab852
engfatmait <eng_fatma_it@yahoo.com>
2023-08-09 14:34:13 -0700

    first commit of 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that adds positive numbers
+ * @argc: number of arguments
+ * @argv: value of arguments
+ * Return: 0 - success
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	long int sum = 0;
+
+	if (argc == 0)
+		printf("%d\n", 0);
+	for (i = 1; i < argc; i++)
+	{
+		if (atol(argv[i]) <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atol(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
