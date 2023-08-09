959c46e865c12ae05d29d1943e9c6c1154edf449
engfatmait <eng_fatma_it@yahoo.com>
2023-08-09 06:38:11 -0700

    first commit of 2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: an integer
+*/
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", argv++);
+	return (0);
+}
