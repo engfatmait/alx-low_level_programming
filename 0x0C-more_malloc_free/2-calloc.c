0c9d3c2cae2153f66ca810da6cca499a243e18ab
engfatmait <eng_fatma_it@yahoo.com>
2023-08-10 13:35:26 -0700

    first commit of 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ * _calloc - a function that allocates memory for an array, using malloc.
+ * @nmemb: number of elements for the array
+ * @size: number of bytes
+ * Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(sizeof(size) * nmemb);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < sizeof(size) * nmemb; i++)
+		a[i] = 0;
+	return (a);
+}
