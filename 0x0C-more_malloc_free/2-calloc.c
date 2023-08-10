c80c8630e9bb8415d1fdcbec99e9aeff5838d698
engfatmait <eng_fatma_it@yahoo.com>
2023-08-10 13:42:56 -0700

    new commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	a = malloc(sizeof(size) * nmemb);
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; i < sizeof(size * nmemb); i++)
+	for (i = 0; i < (size * nmemb); i++)
 		a[i] = 0;
 	return (a);
 }
