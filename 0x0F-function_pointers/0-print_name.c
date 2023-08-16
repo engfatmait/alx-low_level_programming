0c3d8a4c1896dff4fafd2d67007d9192fb5b7e79
engfatmait <eng_fatma_it@yahoo.com>
2023-08-16 12:48:13 -0700

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - function to print name
+ * @name: input name
+ * @f: a pointer to function
+ * Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef _FUN_PTR_
+#define _FUN_PTR
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+#endif
