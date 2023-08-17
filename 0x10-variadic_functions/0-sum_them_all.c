3818e76bdaafdb7a55770427b873df7cee3ffacb
engfatmait <eng_fatma_it@yahoo.com>
2023-08-17 11:53:04 -0700

    add condition if n ==0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,6 +9,8 @@ int sum_them_all(const unsigned int n, ...)
 	int i, sum = 0;
 	va_list args;
 
+	if (n == 0)
+		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
