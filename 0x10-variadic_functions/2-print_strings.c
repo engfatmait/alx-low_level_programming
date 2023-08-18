af9904282579fa4c69894c0fa305722d28f7a7a6
engfatmait <eng_fatma_it@yahoo.com>
2023-08-17 19:59:01 -0700

    first commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		argstr = va_arg(str, char*);
-		if (argstr == NULL)
+		if (*argstr == NULL)
 			printf("(nil)");
 		printf("%s", argstr);
 		if (separator && i < n - 1)
