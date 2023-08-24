4643c264f135f97df88ee97195970ee3f2bd2936
engfatmait <eng_fatma_it@yahoo.com>
2023-08-24 16:35:37 -0700

    first commit of function list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len -  returns the number of elements in a linked list_t list.
+ * @h: singly linked list
+ * Return: length of list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h->next;
+		count++;
+	}
+	return (count);
+
+}
