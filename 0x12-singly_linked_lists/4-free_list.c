1180549214ffad684680c757abca168ae46f3b3e
engfatmait <eng_fatma_it@yahoo.com>
2023-08-24 19:06:20 -0700

    first commit of fun free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - a function that frees a list_t list.
+ * @head: singly linked list
+ * Return: nothig
+*/
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+}
