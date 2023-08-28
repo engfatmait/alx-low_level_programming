a7b8b90c9ebf93cf0371478550774b6769856bd1
engfatmait <eng_fatma_it@yahoo.com>
2023-08-28 12:40:12 -0700

    first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * a function that prints all the elements of a listint_t list
+ * @h: input of type instruct
+ * Return: the number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
