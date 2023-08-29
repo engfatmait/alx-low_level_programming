91fec3b259825af7267127c9fdfa72869b8cb150
engfatmait <eng_fatma_it@yahoo.com>
2023-08-29 11:49:59 -0700

    correcting if clause does not guard

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,9 +15,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-		new->n = n;
-		new->next = *head;
-		*head = new;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
 	return (*head);
 }
