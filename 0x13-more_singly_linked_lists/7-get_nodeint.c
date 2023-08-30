cb3aa9a01cfc3b9d5719a6f44f96716f4978b2f5
engfatmait <eng_fatma_it@yahoo.com>
2023-08-30 09:21:39 -0700

    first commit of fun get node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: head node
+ * @index: index of the required node
+ * Return: the node of given index, NULL if not exist
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	unsigned int i = 0;
+
+	node = head;
+	while (i < index)
+	{
+		node = node->next;
+		i++;
+	}
+	if (node == NULL)
+		return (NULL);
+
+	return (node);
+}
