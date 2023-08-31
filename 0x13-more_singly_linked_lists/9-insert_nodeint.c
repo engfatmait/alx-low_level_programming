c948ae008ce5d609f67122acda0d10cd887ef802
engfatmait <eng_fatma_it@yahoo.com>
2023-08-30 20:01:09 -0700

    new commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,8 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	cur->n = n;
 	if (idx == 0)
 	{
-		cur->next = (*head)->next;
-		(*head)->next = cur;
+		cur->next = *head;
 		*head = cur;
 	}
 	else
