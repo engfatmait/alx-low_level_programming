447ac708c2597d0ad3fd22257d52980e30b04a8a
engfatmait <eng_fatma_it@yahoo.com>
2023-09-03 20:39:35 -0700

    change the return value 0 or 1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,5 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index >= (sizeof(n) * 8))
 		return (-1);
-	return (n & (1 << index));
+	if (n & (1 << index))
+		return (1);
+
+	return (0);
 }
