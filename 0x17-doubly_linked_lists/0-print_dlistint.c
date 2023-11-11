06bbe9bb76e683022060d3984756f66cf6e05213
engfatmait <eng_fatma_it@yahoo.com>
2023-11-11 12:09:36 -0800

    handle prev node not null

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,6 +12,8 @@ size_t print_dlistint(const dlistint_t *h)
 	count = 0;
 	if (h == NULL)
 		return (count);
+	while (h->prev != NULL)
+		h = h->prev;
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
