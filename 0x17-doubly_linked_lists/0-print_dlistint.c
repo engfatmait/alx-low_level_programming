3acbcd44b4d46c753548fe5192e119d9b6157728
engfatmait <eng_fatma_it@yahoo.com>
2023-11-11 12:15:34 -0800

    new commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -14,11 +14,11 @@ size_t print_dlistint(const dlistint_t *h)
 		return (count);
 	while (h->prev != NULL)
 		h = h->prev;
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
-		h = h->next
+		h = h->next;
 	}
 	return (count);
 }
